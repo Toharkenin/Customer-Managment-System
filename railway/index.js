ccdfd678f5699e3fd26dd9bfa62a80e251e06f85
Toharkenin <109432113+Toharkenin@users.noreply.github.com>
2025-03-27 16:19:24 +0200

    add tsconfig.json

--- railway/index.js
+++ railway/index.js
@@ -1,10 +1,9 @@
+"use strict";
 // import express from 'express';
 // import cron from 'node-cron';
 // import admin from 'firebase-admin';
-
 // const app = express();
 // const PORT = process.env.PORT || 3000;
-
 // admin.initializeApp({
 //     credential: admin.credential.cert({
 //       projectId: process.env.FIREBASE_PROJECT_ID,
@@ -12,31 +11,20 @@
 //       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 //     }),
 //   });
-
-
 // const db = admin.firestore();
-
-
 // app.get('/', (req, res) => {
 //   res.send('Cron job is running!');
 // });
-
-
 // cron.schedule('0 10 1 * *', async () => {
 //   console.log('⏰ Running monthly cron job...');
-  
 //   const customersRef = db.collection('customers');
 //   const snapshot = await customersRef.get();
-
 //   snapshot.forEach(doc => {
 //     const customer = doc.data();
 // //test//
 //     console.log(`🔔 Sending reminder to ${customer.name}`);
-    
-   
 //   });
 // });
-
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port ${PORT}`);
 // });
